7e4880eba615624395d662d499d2c587d1f8caa4
ralphsunny22 <ralphsunny114@gmail.com>
2022-04-30 12:21:52 +0100

    redux setup, productsSlice introduction

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import productsReducer from './features/productsSlice';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = configureStore({
+  reducer:{
+    products: productsReducer,
+  }
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
